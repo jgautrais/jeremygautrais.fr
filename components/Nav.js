1a14a65e893d3d56b216f66f1400a01d59815026
jgautrais <j.gautrais@gmail.com>
2021-11-07 14:34:40 +0100

    blog feature init

--- components/Nav.js
+++ components/Nav.js
@@ -55,6 +55,9 @@ const Nav = () => {
         <Link href='/' passHref>
           <MenuLink onClick={toggleMenu}>{t('navbar.home')}</MenuLink>
         </Link>
+        <Link href='/blog' passHref>
+          <MenuLink onClick={toggleMenu}>Blog</MenuLink>
+        </Link>
         <Link href='/illustration' passHref>
           <MenuLink onClick={toggleMenu}>Illustration</MenuLink>
         </Link>
