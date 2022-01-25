afd18d08cf2589bbf505777eea79e262792a8f19
jgautrais <j.gautrais@gmail.com>
2022-01-25 14:06:08 +0100

    aest game description updated

--- components/Projects.js
+++ components/Projects.js
@@ -5,160 +5,178 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Projects = () => {
-  const { t } = useTranslation('home');
-  const projects = t('projectItems', {}, { returnObjects: true });
+    const { t } = useTranslation('home');
+    const projects = t('projectItems', {}, { returnObjects: true });
 
-  const H3 = ({ children }) => {
-    return (
-      <h3 className='text-xl sm:text-2xl font-semibold text-gray-900 dark:text-gray-100 mt-8 mt-0'>
-        {children}
-      </h3>
-    );
-  };
-
-  const Br = (props) => <br {...props} />;
-
-  const ProjectTech = ({ tech }) => {
-    let name = null;
-
-    switch (tech) {
-      case 'html':
-        name = 'html';
-        break;
-      case 'css':
-        name = 'css';
-        break;
-      case 'js':
-        name = 'javascript';
-        break;
-      case 'sass':
-        name = 'sass';
-        break;
-      case 'react':
-        name = 'react';
-        break;
-      case 'nodejs':
-        name = 'node.js';
-        break;
-      case 'mongo':
-        name = 'mongoDB';
-        break;
-      case 'nextjs':
-        name = 'next.js';
-        break;
-      case 'tailwind':
-        name = 'tailwind';
-        break;
-      case 'i18n':
-        name = 'i18n';
-        break;
-      case 'mdx':
-        name = 'mdx';
-        break;
-    }
-
-    return (
-      <span className={`mt-0.5 text-sm mr-2 text-gray-500 dark:text-gray-400`}>
-        <span className='text-gray-300 dark:text-gray-600'>#</span>
-        {name}
-      </span>
-    );
-  };
-
-  const ProjectLink = React.forwardRef(({ children, href }, ref) => (
-    <a
-      ref={ref}
-      href={href}
-      className='block text-sm font-medium text-blue-500 dark:text-blue-400 hover:text-blue-700 dark:hover:text-blue-200 inline-block w-min mt-2 first:mr-20 first:sm:mr-32'
-      target='_blank'
-      rel='noreferrer'
-    >
-      {children}
-    </a>
-  ));
-  ProjectLink.displayName = 'ProjectLink';
-
-  return (
-    <div className='lg:grid lg:grid-cols-2 lg:gap-16 lg:auto-rows-min'>
-      {projects.map((project, i) => {
+    const H3 = ({ children }) => {
         return (
-          <div key={i}>
-            <article
-              key={i}
-              className={`project__container border-2 ${
-                project.live
-                  ? 'border-gray-100 dark:border-gray-500'
-                  : 'border-gray-300 dark:border-gray-100'
-              } dark:border-opacity-25 py-4 md:py-6 px-6 mb-12 rounded-xl max-w-sm md:max-w-2xl lg:max-w-sm mx-auto`}
-            >
-              <H3>{project.title}</H3>
-              <div className='flex flex-wrap'>
-                {project.stack.split(',').map((tech, i) => {
-                  return <ProjectTech tech={tech} key={i}></ProjectTech>;
-                })}
-              </div>
-              <div className='flex flex-col md:flex-row lg:flex-col mt-6'>
-                {project.img ? (
-                  <Link href={project.live} passHref>
-                    <a
-                      className='block mx-auto md:mx-0 lg:mx-auto h-full'
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      <div
-                        className='project__img relative mx-auto md:mx-0 lg:mx-auto flex-grow md:mr-8 lg:mr-0'
-                        style={{
-                          width: '16rem',
-                          maxWidth: '55vw',
-                          height: '13rem',
-                        }}
-                      >
-                        <Image
-                          alt='Project Illustration'
-                          src={project.img}
-                          layout='fill'
-                          objectFit='cover'
-                        />
-                      </div>
-                    </a>
-                  </Link>
-                ) : (
-                  ''
-                )}
-                <Trans
-                  i18nKey={`home:projectItems.${i}.description`}
-                  components={[
-                    <p
-                      key='0'
-                      className=' text-gray-600 dark:text-gray-300 mt-4 md:mt-0 lg:ml-0 lg:mt-4'
-                    />,
-                    <br key='1' />,
-                    <strong
-                      key='2'
-                      className='text-gray-800 dark:text-gray-200'
-                    />,
-                  ]}
-                />
-              </div>
-
-              <div className='mt-4 md:mt-6 flex flex-row justify-between md:block lg:flex'>
-                <Link href={project.github} passHref>
-                  <ProjectLink>Github</ProjectLink>
-                </Link>
-                {project.live ? (
-                  <Link href={project.live} passHref>
-                    <ProjectLink>Live&nbsp;Demo</ProjectLink>
-                  </Link>
-                ) : (
-                  ''
-                )}
-              </div>
-            </article>
-          </div>
+            <h3 className='text-xl sm:text-2xl font-semibold text-gray-900 dark:text-gray-100 mt-8 mt-0'>
+                {children}
+            </h3>
         );
-      })}
-    </div>
-  );
+    };
+
+    const Br = (props) => <br {...props} />;
+
+    const ProjectTech = ({ tech }) => {
+        let name = null;
+
+        switch (tech) {
+            case 'html':
+                name = 'html';
+                break;
+            case 'css':
+                name = 'css';
+                break;
+            case 'js':
+                name = 'javascript';
+                break;
+            case 'sass':
+                name = 'sass';
+                break;
+            case 'react':
+                name = 'react';
+                break;
+            case 'nodejs':
+                name = 'node.js';
+                break;
+            case 'mongo':
+                name = 'mongoDB';
+                break;
+            case 'nextjs':
+                name = 'next.js';
+                break;
+            case 'tailwind':
+                name = 'tailwind';
+                break;
+            case 'i18n':
+                name = 'i18n';
+                break;
+            case 'mdx':
+                name = 'mdx';
+                break;
+            case 'symfony':
+                name = 'symfony';
+                break;
+            case 'mysql':
+                name = 'mySQL';
+                break;
+            case 'php':
+                name = 'php';
+                break;
+        }
+
+        return (
+            <span
+                className={`mt-0.5 text-sm mr-2 text-gray-500 dark:text-gray-400`}
+            >
+                <span className='text-gray-300 dark:text-gray-600'>#</span>
+                {name}
+            </span>
+        );
+    };
+
+    const ProjectLink = React.forwardRef(({ children, href }, ref) => (
+        <a
+            ref={ref}
+            href={href}
+            className='block text-sm font-medium text-blue-500 dark:text-blue-400 hover:text-blue-700 dark:hover:text-blue-200 inline-block w-min mt-2 first:mr-20 first:sm:mr-32'
+            target='_blank'
+            rel='noreferrer'
+        >
+            {children}
+        </a>
+    ));
+    ProjectLink.displayName = 'ProjectLink';
+
+    return (
+        <div className='lg:grid lg:grid-cols-2 lg:gap-16 lg:auto-rows-min'>
+            {projects.map((project, i) => {
+                return (
+                    <div key={i}>
+                        <article
+                            key={i}
+                            className={`project__container border-2 ${
+                                project.live
+                                    ? 'border-gray-100 dark:border-gray-500'
+                                    : 'border-gray-300 dark:border-gray-100'
+                            } dark:border-opacity-25 py-4 md:py-6 px-6 mb-12 rounded-xl max-w-sm md:max-w-2xl lg:max-w-sm mx-auto`}
+                        >
+                            <H3>{project.title}</H3>
+                            <div className='flex flex-wrap'>
+                                {project.stack.split(',').map((tech, i) => {
+                                    return (
+                                        <ProjectTech
+                                            tech={tech}
+                                            key={i}
+                                        ></ProjectTech>
+                                    );
+                                })}
+                            </div>
+                            <div className='flex flex-col md:flex-row lg:flex-col mt-6'>
+                                {project.img ? (
+                                    <Link href={project.live} passHref>
+                                        <a
+                                            className='block mx-auto md:mx-0 lg:mx-auto h-full'
+                                            target='_blank'
+                                            rel='noreferrer'
+                                        >
+                                            <div
+                                                className='project__img relative mx-auto md:mx-0 lg:mx-auto flex-grow md:mr-8 lg:mr-0'
+                                                style={{
+                                                    width: '16rem',
+                                                    maxWidth: '55vw',
+                                                    height: '13rem',
+                                                }}
+                                            >
+                                                <Image
+                                                    alt='Project Illustration'
+                                                    src={project.img}
+                                                    layout='fill'
+                                                    objectFit='cover'
+                                                />
+                                            </div>
+                                        </a>
+                                    </Link>
+                                ) : (
+                                    ''
+                                )}
+                                <Trans
+                                    i18nKey={`home:projectItems.${i}.description`}
+                                    components={[
+                                        <p
+                                            key='0'
+                                            className=' text-gray-600 dark:text-gray-300 mt-4 md:mt-0 lg:ml-0 lg:mt-4'
+                                        />,
+                                        <br key='1' />,
+                                        <strong
+                                            key='2'
+                                            className='text-gray-800 dark:text-gray-200'
+                                        />,
+                                    ]}
+                                />
+                            </div>
+
+                            <div className='mt-4 md:mt-6 flex flex-row justify-between md:block lg:flex'>
+                                <Link href={project.github} passHref>
+                                    <ProjectLink>Github</ProjectLink>
+                                </Link>
+                                {project.live ? (
+                                    <Link href={project.live} passHref>
+                                        <ProjectLink>
+                                            Live&nbsp;Demo
+                                        </ProjectLink>
+                                    </Link>
+                                ) : (
+                                    ''
+                                )}
+                            </div>
+                        </article>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Projects;
--- components/Skills.js
+++ components/Skills.js
@@ -1,60 +1,60 @@
 import useTranslation from 'next-translate/useTranslation';
 
 const Skills = () => {
-  const { t } = useTranslation('home');
+    const { t } = useTranslation('home');
 
-  const H3 = ({ children }) => {
+    const H3 = ({ children }) => {
+        return (
+            <h3 className='text-2xl font-medium text-gray-500 dark:text-gray-400 mt-8 mb-2 mt-0'>
+                {children}
+            </h3>
+        );
+    };
+
+    const PSkills = ({ children }) => {
+        return (
+            <p className='mt-1.5 fonnt-normal flex items-center'>
+                <span className='d-block w-2 h-0.5 ml-3 mr-2 mb-0.5 bg-gradient-to-r from-blue-300 dark:from-blue-600 to-blue-500 rounded-sm'></span>
+                {children}
+            </p>
+        );
+    };
+
     return (
-      <h3 className='text-2xl font-medium text-gray-500 dark:text-gray-400 mt-8 mb-2 mt-0'>
-        {children}
-      </h3>
+        <>
+            <div className='md:grid md:grid-cols-2' id='skills'>
+                <div className='mb-6'>
+                    <H3>Front-End</H3>
+                    <PSkills>HTML5</PSkills>
+                    <PSkills>CSS/Sass, Bootstrap, Tailwind</PSkills>
+                    <PSkills>JavaScript, ES6, jQuery</PSkills>
+                    <PSkills>React</PSkills>
+                </div>
+                <div className='mb-6'>
+                    <H3>Back-End</H3>
+                    <PSkills>PHP, Symfony, Twig</PSkills>
+                    <PSkills>Nodejs, Express</PSkills>
+                </div>
+
+                <div className='mb-6'>
+                    <H3>Databases</H3>
+                    <PSkills>mySQL</PSkills>
+                    <PSkills>Doctrine ORM</PSkills>
+                </div>
+
+                <div className='mb-6'>
+                    <H3>Design</H3>
+                    <PSkills>Figma</PSkills>
+                    <PSkills>Adobe Illustrator, Affinity Designer</PSkills>
+                </div>
+
+                <div>
+                    <H3>{t('skills.tools')}</H3>
+                    <PSkills>git, npm, yarn, composer</PSkills>
+                </div>
+            </div>
+        </>
     );
-  };
-
-  const PSkills = ({ children }) => {
-    return (
-      <p className='mt-1.5 fonnt-normal flex items-center'>
-        <span className='d-block w-2 h-0.5 ml-3 mr-2 mb-0.5 bg-gradient-to-r from-blue-300 dark:from-blue-600 to-blue-500 rounded-sm'></span>
-        {children}
-      </p>
-    );
-  };
-
-  return (
-    <>
-      <div className='md:grid md:grid-cols-2' id='skills'>
-        <div className='mb-6'>
-          <H3>Front-End</H3>
-          <PSkills>HTML5</PSkills>
-          <PSkills>CSS/Sass, Bootstrap, Tailwind</PSkills>
-          <PSkills>JavaScript, ES6, jQuery</PSkills>
-          <PSkills>React</PSkills>
-        </div>
-        <div className='mb-6'>
-          <H3>Back-End</H3>
-          <PSkills>Nodejs, Express</PSkills>
-          <PSkills>PHP, Symfony, Twig</PSkills>
-        </div>
-
-        <div className='mb-6'>
-          <H3>Databases</H3>
-          <PSkills>MongoDB</PSkills>
-          <PSkills>mySQL</PSkills>
-        </div>
-
-        <div className='mb-6'>
-          <H3>Design</H3>
-          <PSkills>Figma</PSkills>
-          <PSkills>Adobe Illustrator, Affinity Designer</PSkills>
-        </div>
-
-        <div>
-          <H3>{t('skills.tools')}</H3>
-          <PSkills>git, npm, composer</PSkills>
-        </div>
-      </div>
-    </>
-  );
 };
 
 export default Skills;
