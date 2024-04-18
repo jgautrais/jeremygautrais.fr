b163f3351af88baf5dec5c84076bd604ded662bb
Jeremy Gautrais <j.gautrais@thecodingmachine.com>
2024-04-18 18:21:20 +0200

    adjusting landing

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -11,8 +11,15 @@ export default async function Home({
 
   return (
     <PageTemplate lang={lang}>
-      <h1 className="font-hero text-teal-500 text-4xl md:text-6xl font-bold text-start mb-4 md:mb-6">Jérémy Gautrais</h1>
-      <h2 className="text-xl md:text-3xl text-start">Développeur web Fullstack</h2>
+      <div className="flex">
+        <div className="bg-teal-400 w-1.5 md:w-2 me-4 md:mx-8 rounded">
+        </div>
+        <div>
+          <h1 className="font-hero text-4xl md:text-7xl font-bold text-start mb-3 md:mb-6">Jérémy
+            Gautrais</h1>
+          <h2 className="text-teal-600 text-xl md:text-4xl text-start">{dictionary['hero'].job}</h2>
+        </div>
+      </div>
     </PageTemplate>
   );
 }
